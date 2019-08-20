fd04e5c5c38500d50114f45e175420f4d286863b
Ronan <RonanLuo@boyaa.com>
2019-08-20 19:35:26 +0800

    feat: 客户端更新

---
+++ app/src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import '@/icons'
+import Element from 'element-ui'
+import './styles/element-variables.scss'
+import '@/styles/index.scss' // global css
+
+Vue.use(Element, {
+  // size: Cookies.get('size') || 'medium',
+  // i18n: (key, value) => i18n.t(key, value)
+})
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ app/src/views/Login.vue
@@ -0,0 +1,144 @@
+<template>
+    <div class="login-container">
+        <el-form class="login-form" label-position="left">
+            <div class="title-container">
+                <h3 class="title">Login Form</h3>
+            </div>
+
+            <el-form-item>
+                <span class="svg-container">
+                    <svg-icon icon-class="user" />
+                </span>
+                <el-input
+                    placeholder="Username"
+                    name="username"
+                    type="text"
+                    tabindex="1"
+                    autocomplete="on"
+                />
+            </el-form-item>
+
+            <el-form-item>
+                <span class="svg-container">
+                    <svg-icon icon-class="password" />
+                </span>
+                <el-input
+                    placeholder="Password"
+                    name="password"
+                    tabindex="2"
+                    autocomplete="on"
+                />
+            </el-form-item>
+        </el-form>
+    </div>
+</template>
+
+<style lang="scss">
+/* 修复input 背景不协调 和光标变色 */
+/* Detail see https://github.com/PanJiaChen/vue-element-admin/pull/927 */
+
+$bg:#283443;
+$light_gray:#fff;
+$cursor: #fff;
+
+@supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
+  .login-container .el-input input {
+    color: $cursor;
+  }
+}
+
+/* reset element-ui css */
+.login-container {
+  .el-input {
+    display: inline-block;
+    height: 47px;
+    width: 85%;
+
+    input {
+      background: transparent;
+      border: 0px;
+      -webkit-appearance: none;
+      border-radius: 0px;
+      padding: 12px 5px 12px 15px;
+      color: $light_gray;
+      height: 47px;
+      caret-color: $cursor;
+
+      &:-webkit-autofill {
+        box-shadow: 0 0 0px 1000px $bg inset !important;
+        -webkit-text-fill-color: $cursor !important;
+      }
+    }
+  }
+
+  .el-form-item {
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    background: rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+    color: #454545;
+  }
+}
+</style>
+
+<style lang="scss" scoped>
+$bg:#2d3a4b;
+$dark_gray:#889aa4;
+$light_gray:#eee;
+
+.login-container {
+  min-height: 100%;
+  width: 100%;
+  background-color: $bg;
+  overflow: hidden;
+
+  .login-form {
+    position: relative;
+    width: 520px;
+    max-width: 100%;
+    padding: 160px 35px 0;
+    margin: 0 auto;
+    overflow: hidden;
+  }
+
+  .svg-container {
+    padding: 6px 5px 6px 15px;
+    color: $dark_gray;
+    vertical-align: middle;
+    width: 30px;
+    display: inline-block;
+  }
+
+  .title-container {
+    position: relative;
+
+    .title {
+      font-size: 26px;
+      color: $light_gray;
+      margin: 0px auto 40px auto;
+      text-align: center;
+      font-weight: bold;
+    }
+
+    .set-language {
+      color: #fff;
+      position: absolute;
+      top: 3px;
+      font-size: 18px;
+      right: 0px;
+      cursor: pointer;
+    }
+  }
+
+  .thirdparty-button {
+    position: absolute;
+    right: 0;
+    bottom: 6px;
+  }
+
+  @media only screen and (max-width: 470px) {
+    .thirdparty-button {
+      display: none;
+    }
+  }
+}
+</style>
